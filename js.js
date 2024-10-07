f15195094bfbb4d660395b55221c3106b4df384f
osamaabdallah225 <osamaabdallah225@gmail.com>
2024-10-07 12:26:23 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -442,7 +442,7 @@ document.getElementById("menuIcon").addEventListener("click", function() {
 // --------------------------------------------------------------------------------------------
 
  // دالة تشيك علي التحديث
-
+ // كود قديم اللي تحت افضل منه هي نفش الفكرة
 // function checkUpdateNumber() {
 //     if (localStorage.getItem("update") === null) { 
 //         localStorage.setItem("update", 2);
@@ -464,7 +464,7 @@ function checkUpdateNumber() {
 
 
 // تعيين نسخة جديدة للتطبيق في localStorage
-const appVersion = '1.0.5';
+const appVersion = '1.0.2';
 const savedVersion = localStorage.getItem('appVersion');
 
 if (savedVersion !== appVersion) {
@@ -472,21 +472,8 @@ if (savedVersion !== appVersion) {
     alert("تم عمل تحديث للأصدار");
     window.location.reload();
     localStorage.removeItem('temporaryData');
-    localStorage.setItem('appVersion', appVersion);
-     // مسح الكاش
-    //  if ('caches' in window) {
-    //     caches.keys().then(function(cacheNames) {
-    //         cacheNames.forEach(function(cacheName) {
-    //             caches.delete(cacheName).then(function(success) {
-                    
-    //             });
-    //         });
-    //     });
+    localStorage.setItem('appVersion', appVersion);  // تحديث بالقيمة الجديدة للأصدار
 
-    // // إذا كانت النسخة المحفوظة مختلفة، قم بمسح الكاش المخصص للتطبيق
-    
-    // }
-    // // يمكنك إضافة أي بيانات أخرى ترغب في مسحها هنا
 }
 
 
