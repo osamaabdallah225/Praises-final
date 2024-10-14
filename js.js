51a477a803ce1e89b061cefd3f05c9c94d0c83a5
osamaabdallah225 <osamaabdallah225@gmail.com>
2024-10-14 23:18:31 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -430,76 +430,132 @@ document.getElementById("bold").addEventListener("click", function() {
 //  نهاية الاعدادات
 
 // وضع الموبايل على الصفحة
+// ********************
+// ********************
+// ********************
+// ********************
+// ********************
 
 if (window.innerWidth  < 600) {
-// متغير لتخزين حالة ظهور الأزرار
-let areButtonsVisible = false;
-// دالة لإخفاء جميع العناصر ما عدا العنصر الذي تم النقر عليه وتثبيته في أعلى الصفحة
-function hideOthersAndFix(event) {
-    // التكرار عبر جميع العناصر ذات الكلاس 'main-content9'
-    const allButtons = document.querySelectorAll('.main-button');
-    allButtons.forEach((el) => {
-        // إخفاء العنصر إذا لم يكن هو العنصر الذي تم النقر عليه
-        if (el !== event.target) {
-            el.style.display = 'none';
+    // متغير لتخزين حالة ظهور الأزرار
+    // let areButtonsVisible = false;
+    // دالة لإخفاء جميع العناصر ما عدا العنصر الذي تم النقر عليه وتثبيته في أعلى الصفحة
+    let activeButtonIndex = -1; // لحفظ الزر النشط
+    let isButtonActive = true; // حالة الزر (مفعل أو غير مفعل)
+    
+    function hideOthersAndFix(event) {
+        const allButtons = document.querySelectorAll('.main-button');
+    
+        allButtons.forEach((el, index) => {
+            // إخفاء العنصر إذا لم يكن هو العنصر الذي تم النقر عليه
+            if (el !== event.target) {
+                el.style.display = 'none';
+                
+            } else {
+                if (isButtonActive) {
+                    checkUpdateNumber(); // دالة تشيك علي التحديث
+    
+                    // تثبيت العنصر في أعلى الصفحة
+                    el.style.position = 'fixed';
+                    el.style.top = '0px';
+                    el.style.left = '50%';
+                    el.style.transform = 'translateX(-50%)';
+                    el.style.zIndex = '10';
+                    el.style.margin = '0px 0px 50px 0px';
+                    el.style.pointerEvents = 'none'; // تعطيل الضغط على الزر المختار
+                    isButtonActive = false; // تعطيل الزر بعد الضغط عليه
+                    activeButtonIndex = index; // تحديث الزر النشط
+                    el.style.boxShadow = '';
+                    el.style.backgroundColor = '';
+                    document.body.style.paddingTop = '60px';
 
 
-        } else {
-            checkUpdateNumber()   // دالة تشيك علي التحديث
-            // تثبيت العنصر في أعلى الصفحة
-            el.style.position = 'fixed';
-            el.style.top = '0'; // لضمان أنه سيكون في أعلى الصفحة
-            el.style.left = '50%'; // لضمان أنه سيكون في منتصف العرض
-            el.style.transform = 'translateX(-50%)'; // لضمان تموضع العنصر في المنتصف
-            el.style.zIndex = '10'; // لجعل العنصر يظهر فوق باقي العناصر
-            el.style.margin = '0px 0px 50px 0px';
-            el.style.backgroundColor = '';
-            el.style.boxShadow = '';
-            document.body.style.paddingTop = '60px';
-            
-            // el.style.pointerEvents = 'none'; // تعطيل الضغط على الزر المختار في الموبايل فقط
-        }
-    });
-}
+                    
+         
+                   
+                }
+            }
+        });
+    }
+    
+
 // إضافة مستمع للأحداث لجميع العناصر التي تحتوي على الكلاس 'main-button'
 document.querySelectorAll('.main-button').forEach((el) => {
     el.addEventListener('click', hideOthersAndFix);
 });
+
+// **********************************************************************
+
+//عند الضغط علي menuIcon 
 // إضافة مستمع لحدث الضغط على زر "menuIcon"
-document.getElementById("menuIcon").addEventListener("click", function() {
-    // التكرار عبر جميع العناصر التي تحتوي على الكلاس 'main-button'
-    const allButtons = document.querySelectorAll('.main-button');
-    document.body.style.paddingTop = '0px';
-    document.getElementById("finished").style.display = 'none';  // اخفاء رسالة الانتهاء من الذكر
-    document.getElementById("progress-container").style.display = 'none';  // إخفاء شريط التقديم 
-    hideAll()  
+// تعريف متغير لتتبع النقر على الأزرار الفرعية button-sub
+let actionOccurredOnSubButton = false;
 
-
-        // إذا كانت الأزرار مخفية، نقوم بإظهارها
-        allButtons.forEach(function(element) {
-            element.style.display = "block"; // إظهار الأزرار
-            // إعادة وضع الزر الذي تم تثبيته إلى الوضع العادي (لا يعود إلى الأعلى)
-            element.style.position = '';
-            element.style.top = '';
-            element.style.left = '';
-            element.style.transform = '';
-            element.style.zIndex = '';
-            element.style.margin = '';
-            element.style.backgroundColor = '';
-            element.style.boxShadow = '';
-        });
-    // تغيير الحالة
-    areButtonsVisible = !areButtonsVisible;
+// إضافة مستمع للنقر على الأزرار داخل main-content1 و main-content2
+document.querySelectorAll('.button-sub').forEach(function(button) {
+    button.addEventListener('click', function() {
+        actionOccurredOnSubButton = true; // تم النقر على زر من الفئة button-sub
+    });
 });
 
+// إضافة مستمع لحدث الضغط على زر "menuIcon"
 document.getElementById("menuIcon").addEventListener("click", function() {
     document.getElementById("menuIcon").style.display = 'none';
+// نتحقق إذا كانت الأذكار قد انتهت
+    const finished = document.getElementById("finished").style.display !== 'none'; 
+    // نتحقق مما إذا حدث أي نشاط على زر من الفئة button-sub
+    const noActionOccurred = !actionOccurredOnSubButton; // إذا لم يتم النقر على أي زر من فئة button-sub
+
+    // إذا لم يحدث أي نشاط على الأزرار الفرعية يتم الخروج مباشرة بدون رسالة
+    if (noActionOccurred || finished) {
+        exitWithoutConfirmation();
+    } else {
+        // إذا تم النقر على زر من فئة button-sub، نسأل المستخدم ما إذا كان يريد الخروج
+        const userConfirmed = confirm("تم البدء في قراءة الذكر. هل تريد الخروج ؟");
+        if (userConfirmed === true) {
+            exitWithoutConfirmation();
+        } else {
+            // إعادة عرض القائمة إذا لم يؤكد المستخدم الخروج
+            document.getElementById("menuIcon").style.display = 'block';
+        }
+    }
+});
+
+// دالة لتنفيذ الخروج بدون رساله تأكيد
+function exitWithoutConfirmation() {
+    const allButtons = document.querySelectorAll('.main-button');
+    document.body.style.paddingTop = '0px';
+    document.getElementById("finished").style.display = 'none'; // اخفاء رسالة الانتهاء من الذكر
+    document.getElementById("progress-container").style.display = 'none'; // إخفاء شريط التقديم 
+    // عند الصغط علي الاسقوتة هيتم الذهاب الي اعلي الصفحة
     window.scrollTo({
-        top: 0,
-        behavior: 'smooth' // للتأكد من أن التمرير سلس (اختياري)
+                top: 0,
+                behavior: 'smooth' // للتأكد من أن التمرير سلس (اختياري)
+            });
+            // اخفاء جميع عناصر main-content1 , 2 , .........
+    hideAll();
+
+    // إذا كانت الأزرار مخفية، نقوم بإظهارها
+    allButtons.forEach(function(element) {
+        element.style.display = "block"; // إظهار الأزرار
+        // إعادة وضع الزر الذي تم تثبيته إلى الوضع العادي
+        element.style.position = '';
+        element.style.top = '';
+        element.style.left = '';
+        element.style.transform = '';
+        element.style.zIndex = '';
+        element.style.margin = '';
+        element.style.pointerEvents = 'auto'; // إعادة تفعيل الضغط على الزر
     });
-})
+
+    isButtonActive = true; // إعادة تفعيل الزر
+
+    // إعادة تعيين حالة النقر على الأزرار
+    actionOccurredOnSubButton = false;
 }
+}
+
+
 
 // --------------------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------------------
@@ -526,12 +582,12 @@ document.getElementById("menuIcon").addEventListener("click", function() {
 
 function checkUpdateNumber() {
 // تعيين نسخة جديدة للتطبيق في localStorage
-const appVersion = '1.4.0';
+const appVersion = '1.5.0';
 const savedVersion = localStorage.getItem('appVersion');
 
 if (savedVersion !== appVersion) {
  // مسح البيانات المؤقتة أو الكاش الخاص بالتطبيق فقط (وليس كل localStorage)
-    // alert("تم عمل تحديث للأصدار");
+    alert("تم عمل تحديث للأصدار");
     window.location.reload();
     localStorage.removeItem('temporaryData');
     localStorage.setItem('appVersion', appVersion);  // تحديث بالقيمة الجديدة للأصدار
